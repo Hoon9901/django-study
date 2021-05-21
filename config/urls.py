0fa8da822680271cff6a2a3787c9e10b3a10c5cd
Hooooooon <shonn2323@gmail.com>
2021-05-21 20:22:20 +0900

    추천 기능 추가

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from pybo.views import base_view
+from pybo.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
-    path('', base_view.index, name = 'index'),
+    path('', base_views.index, name = 'index'),
 ]
--- pybo/models.py
+++ pybo/models.py
@@ -5,12 +5,13 @@ from django.db.models.deletion import CASCADE # username, password
 # Create your models here.
 # 질문 모델
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE) # ondelete -> 계정 삭제 시 다 삭제
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question') # ondelete -> 계정 삭제 시 다 삭제
     subject = models.CharField(max_length=200)  # 질문 제목
     content = models.TextField()                # 질문 내용
     create_date = models.DateTimeField()        # 작성 일시
     modify_date = models.DateTimeField(null = True, blank=True) # 수정 일시, null,blan -> 어떤 조건으로든 값을 비울숟있다
     view = models.PositiveBigIntegerField(default = 0)
+    voter = models.ManyToManyField(User, related_name='voter_question') # some_user.voter_question.get or all() 으로 접근
 
     @property #템플릿에 사용하기 위해
     def update_view(self) :
@@ -26,11 +27,12 @@ class Question(models.Model):
 class Answer(models.Model):
     # ForeignKey -> 다른 모델과의 연결
     # on_delete = models.CASCADE 답변에 연결된 질문 삭제시 답변도 함께 삭제
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
     question = models.ForeignKey(Question, on_delete=models.CASCADE)    # 질문
     content = models.TextField()                # 답변 내용
     create_date = models.DateTimeField()        # 작성 일시
     modify_date = models.DateTimeField(null=True, blank=True)   # 수정 일시
+    voter = models.ManyToManyField(User, related_name='voter_answer')
 
 # 댓글 모델
 class Comment(models.Model):

0d51e7adc4248045e7546ac01d2349695925a6e1
Hooooooon <shonn2323@gmail.com>
2021-05-25 19:55:00 +0900

    검색 기능 추가

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,20 +1,28 @@
-import django
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from ..models import Question, Answer
+from ..models import Question
+from django.db.models import Q
 
 def index(request):
     """ 목록 출력 """
     # 입력 파라미터, localhost:8000/pybo/?page=1
     page = request.GET.get('page', '1') # 페이지
+    kw = request.GET.get('kw', '')      # 검색어
     # 조회
     question_list = Question.objects.order_by('-create_date')   # 작성일시의 역순 정렬
+    if kw : # 검색어 존재할시
+        question_list = question_list.filter(   # 필드 접근은 __ 사용, icontains -> 대소문자 구별안함
+            Q(subject__icontains = kw) |        # 제목 
+            Q(content__icontains = kw) |        # 내용
+            Q(author__username__icontains = kw) |   # 글쓴이
+            Q(answer__author__username__icontains = kw)   # 답변 글쓴이
+        ).distinct()    # 중복 제거 # Q(answer_content_icontain) <- 답변 내용
     # 페이징처리
     paginator = Paginator(question_list, 10) # 페이지당 10개 씩
     page_obj = paginator.get_page(page)
     last_page = paginator.num_pages
 
-    context = {'question_list' : page_obj, 'last_page' : last_page}
+    context = {'question_list' : page_obj, 'page' : page, 'kw' : kw, 'last_page' : last_page}
     return render(request, 'pybo/question_list.html', context)  # 템플릿
 
 def detail(request, question_id):
